fcc0dd9ce308c7cbc6c55008bc7049bdfb472e21
Branden <brandenehman45@gmail.com>
2021-08-01 13:38:06 -0600

    Working on countdown to stop it from going negative

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const countDown = setInterval(() => {
+      setTimeout(() => setTimeRemaining(timeRemaining - 1), 1000);
+      if(timeRemaining <= 0) {
+        return function cleanup() {
+          clearInterval(timeRemaining);
+        };
+      }
+    });
+
+
+
+    // return function cleanup() {
+    //   clearInterval(countDown);
+    // };
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
